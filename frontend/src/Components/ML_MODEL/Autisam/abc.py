fb910111e3ceacc2e943df7fbf8a3143edc1889d
Bhanu Prakash <kbhanu5125@gmail.com>
2024-02-29 20:53:16 +0530

    Update

--- frontend/src/Components/ML_MODEL/Autisam/abc.py
+++ frontend/src/Components/ML_MODEL/Autisam/abc.py
@@ -10,6 +10,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
-
-
-pickle.dump(regressor,open("ML/ml.pkl","wb"))
+with open("ML/ml.pkl","wb") as fout:
+    pickle.dump(regressor,fout)
