73c799a2e08ac8c3908f9c7eb3b4ff2d05993243
Bhanu Prakash <kbhanu5125@gmail.com>
2024-02-29 12:32:48 +0530

    Update

---
+++ frontend/src/Components/GamePages/Gamepage.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Gamepage.css"; // Create a separate CSS file for styling
+import AnagramCP from "./assests/Anagram_cp.jpg";
+import ColourPatternCP from "./assests/ColourPatterns_cp.jpg.png";
+import EmotionCP from "./assests/EmotionCover_pic.jpg";
+import slidePuzzleCP from "./assests/slide puzzle.jpg";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Gamepage() {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className="gamepage-container">
+      <h1 className="d-flex align-items-center justify-content-center mt-5">
+        Autism
+      </h1>
+      <div className="row1">
+        <div className="boxgamepage">
+          <Link to="/AnagramGame">
+          <img className="imganagram" src={AnagramCP}></img>
+          </Link>
+        </div>
+        <div className="boxgamepage">
+        <Link to="/ColourGame">
+          <img className="imgcolourpattern" src={ColourPatternCP}></img>
+        </Link>
+        </div>
+        <div className="boxgamepage">
+        <Link to="/EmotionGame">
+          <img className="imgemotion" src={EmotionCP}></img>
+        </Link>
+        </div>
+      </div>
+
+      <h1 className="d-flex align-items-center justify-content-center ">
+        Dyslexia
+      </h1>
+      <div className="row1">
+        <div className="boxgamepage">
+        <Link to="/PuzzleGame">
+          <img className="imgslide" src={slidePuzzleCP}></img>
+         </Link>
+        </div>
+        <div className="boxgamepage">
+        <Link to="/WackGame">
+          <img className="wackamole" src={"https://img.gamedistribution.com/5905642773bc49738888210d0b2d3112-512x512.jpeg"}></img>
+        </Link>
+        </div>
+        <div className="boxgamepage">
+        <Link to="/MemoryGame">
+          <img className="memorygame" src={"https://img.freepik.com/free-vector/hand-drawn-memory-game-card_23-2150138543.jpg?size=626&ext=jpg&ga=GA1.1.1368074558.1705160837&semt=ais"}></img>
+        </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
